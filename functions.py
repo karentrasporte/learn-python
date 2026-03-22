eafc07b8a0cb712c778c3651f0b98f6d97e88602
Karen Trasporte <karentrasporte@macbookpro.local>
2026-03-22 16:33:06 +0800

    Using external libraries, adding basic GUI

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """Read to todos.txt file"""
     with open(filepath, "r") as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath="todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """Write to todos.txt file"""
     with open(filepath, "w") as file:
         file.writelines(todos_arg)

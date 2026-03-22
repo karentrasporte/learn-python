3b5cf3ef3b3e0c5853334cc1981ba5a05bb4e986
Karen Trasporte <karentrasporte@macbookpro.local>
2026-03-22 11:30:12 +0800

    Created a module for the functions

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """Read to todos.txt file"""
-    with open(filepath, "r") as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos(todos_arg, filepath="todos.txt"):
-    """Write to todos.txt file"""
-    with open(filepath, "w") as file:
-        file.writelines(todos_arg)
-
+import functions
 
 while True:
     user_action = input("Type add, show, edit, complete or exit:")
@@ -17,12 +6,12 @@ while True:
 
     if user_action.startswith("add"):
         todo = user_action[4:]
-        todos = get_todos()
+        todos = functions.get_todos()
         todos.append(todo + "\n")
-        write_todos(todos)
+        functions.write_todos(todos)
 
     elif user_action.startswith("show"):
-        todos = get_todos()
+        todos = functions.get_todos()
         for index, item in enumerate(todos):
             item = item.strip("\n")
             row = f"{index+1}-{item}"
@@ -32,10 +21,10 @@ while True:
         try:
             number = int(user_action[5:])
             number = number - 1
-            todos = get_todos()
+            todos = functions.get_todos()
             new_todo = input("Enter your new to do:")
             todos[number] = new_todo + "\n"
-            write_todos(todos)
+            functions.write_todos(todos)
 
         except ValueError:
             print("Invalid input")
@@ -44,11 +33,11 @@ while True:
     elif user_action.startswith("complete"):
         try:
             number = int(user_action[9:])
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number -1
             todo_rmv = todos[index].strip("\n")
             todos.pop(index)
-            write_todos(todos)
+            functions.write_todos(todos)
 
             print(f"To-do {todo_rmv} was removed from the list!")
 

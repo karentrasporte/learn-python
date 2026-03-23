168ee7b85924f528207811e5535c08f6a97e95de
karentrasporte <karentrasporte@gmail.com>
2026-03-23 16:30:39 +0800

    Added functionality for 'Add' todo

--- gui.py
+++ gui.py
@@ -1,10 +1,28 @@
-import functions
+from os import write
 import FreeSimpleGUI as sg
+from functions import get_todos, write_todos
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter to-do")
+input_box = sg.InputText(tooltip="Enter to-do", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window(title="Free Simple GUI", layout=[[label],[input_box,add_button]])
-window.read()
+window = sg.Window(title="Free Simple GUI",
+                   layout=[[label],[input_box,add_button]],
+                   font=("Helvetica",20))
+
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = get_todos()
+            new_todo = values['todo'] +"\n"
+            todos.append(new_todo)
+            print(new_todo)
+            write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
